d0e45226a177fe867801683c80c0807f81a6b1d3
jossduff <jduff360@gmail.com>
2025-04-08 14:31:56 -0400

    main logic for contemplant proving

--- hierophant/contemplant/src/types.rs
+++ hierophant/contemplant/src/types.rs
@@ -1,5 +1,37 @@
+use alloy_primitives::B256;
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 use serde::{Deserialize, Serialize};
-use sp1_sdk::network::proto::network::ExecutionStatus;
+use sp1_sdk::{
+    SP1Stdin,
+    network::proto::network::{ExecutionStatus, ProofMode, RequestProofRequestBody},
+};
+use std::{collections::HashMap, fmt::Display};
+use tokio::sync::RwLock;
+
+pub type ProofStore = RwLock<HashMap<B256, ProofStatus>>;
+
+// TODO: (maybe) Gas limit and cycle limit
+#[derive(Serialize, Deserialize)]
+pub struct ProofRequest {
+    pub proof_id: B256,
+    pub elf: Vec<u8>,
+    pub mock: bool,
+    pub mode: ProofMode,
+    pub sp1_stdin: SP1Stdin,
+}
+
+impl Display for ProofRequest {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let proof_id = self.proof_id;
+        let mock = self.mock;
+        let mode = self.mode.as_str_name();
+
+        write!(f, "proof_id: {proof_id}, mock: {mock}, mode: {mode}",)
+    }
+}
 
 /*
 pub enum ExecutionStatus {
@@ -13,33 +45,10 @@ pub enum ExecutionStatus {
 }
 */
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct ProofStatus {
     execution_status: i32,
     proof: Option<Vec<u8>>,
-}
-
-impl Display for ProofStatus {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let execution_status = match self.execution_status {
-            0 => "UnspecifiedExecutionStatus",
-            1 => "Unexecuted",
-            2 => "Executed",
-            3 => "Unexecutable",
-            _ => "Display Error: Unknown execution execution status",
-        };
-
-        let proof = match self.proof {
-            Some(_) => "some",
-            None => "none",
-        };
-
-        write!(
-            f,
-            "ExecutionStatus: {}, Proof: {}",
-            execution_status, proof_display
-        )
-    }
 }
 
 impl ProofStatus {
@@ -57,10 +66,10 @@ impl ProofStatus {
         }
     }
 
-    pub fn executed(proof: Vec<u8>) -> Self {
+    pub fn executed(proof_bytes: Vec<u8>) -> Self {
         Self {
             execution_status: ExecutionStatus::Executed.into(),
-            proof: Some(proof),
+            proof: Some(proof_bytes),
         }
     }
 
@@ -69,5 +78,41 @@ impl ProofStatus {
             execution_status: ExecutionStatus::Unexecutable.into(),
             proof: None,
         }
+    }
+}
+
+impl Display for ProofStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let execution_status = match self.execution_status {
+            0 => "UnspecifiedExecutionStatus",
+            1 => "Unexecuted",
+            2 => "Executed",
+            3 => "Unexecutable",
+            _ => "Display Error: Unknown execution execution status",
+        };
+
+        let proof = match self.proof {
+            Some(_) => "some",
+            None => "none",
+        };
+
+        write!(f, "ExecutionStatus: {}, Proof: {}", execution_status, proof)
+    }
+}
+
+pub struct AppError(pub anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, format!("{}", self.0)).into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
     }
 }
