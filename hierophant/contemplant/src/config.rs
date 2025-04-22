a12f9a36594412d8c3b133536008e6649b45a205
jossduff <jduff360@gmail.com>
2025-04-22 14:45:58 -0400

    allow passing of moongate url

--- hierophant/contemplant/src/config.rs
+++ hierophant/contemplant/src/config.rs
@@ -6,6 +6,13 @@ pub struct Config {
     pub contemplant_name: String,
     #[serde(default = "default_port")]
     pub port: usize,
+    // If None, then the contemplant spins up a cuda prover docker container
+    #[serde(default = "default_moongate_endpoint")]
+    pub moongate_endpoint: Option<String>,
+}
+
+fn default_moongate_endpoint() -> Option<String> {
+    None
 }
 
 fn default_port() -> usize {
