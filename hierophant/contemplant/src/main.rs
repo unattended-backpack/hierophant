ccdf8f50f7dd6f4f93ce138e3dfeab889d1449d2
jossduff <jduff360@gmail.com>
2025-04-22 20:03:48 -0400

    actually read from old_testament.txt

--- hierophant/contemplant/src/main.rs
+++ hierophant/contemplant/src/main.rs
@@ -41,10 +41,10 @@ async fn main() -> Result<()> {
         .await
         .context("read contemplant.toml file")?;
     let config: Config = toml::de::from_str(&config).context("parse config")?;
-    info!("Starting contemplant {}", config.contemplant_name);
 
     // Set up the SP1 SDK logger.
     utils::setup_logger();
+    info!("Starting contemplant {}", config.contemplant_name);
 
     let cuda_prover = match &config.moongate_endpoint {
         // build with undockerized moongate server
